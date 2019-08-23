bda155d729f685bf87aabe8a987b5523ae00ec88
Emuajiz <emuajiz@gmail.com>
2019-08-23 10:47:26 +0700

    Create p2.r

---
+++ p2.r
@@ -0,0 +1,32 @@
+# 1
+  x0 = 2
+  h = 1e-10
+  #a. 
+    f = function(x){
+      x^2 - 4 * x + 10
+    }
+  #b.
+    f = function(x){
+      x^(1/4)
+    }
+#2
+  #a.
+    y = function(x){
+      sin(5 * x) / cos(5 * x)
+    }
+  #b.
+    f = function(x){
+      3 * x^2 + 6 / 5 * x + log(x)
+    }
+  #c.
+    f = function(x){
+      exp(2 * x^2 + 3 * x + 1
+#3
+  x = (1/6) * pi
+  f = function(x){
+    sin(x) * cos(x)
+  }
+#4
+  f = function(x){
+    4 * x ^ 2 + 5
+  }
