643737389058d5b51a35ce386e97b9c0ca16517e
Emuajiz <emuajiz@gmail.com>
2019-09-06 10:48:12 +0700

    Update p4.r

--- p4.r
+++ p4.r
@@ -5,3 +5,28 @@ f <- function(x){
 }
 sum(tm)
 log(2)
+
+g <- function(x){
+  if(x>=1.0){
+    2^((-1*x)+4)*3
+  }
+}
+
+h <- function(x){
+  if(x==1.0) {return(0.6)}
+  else if(x>1.0) {return(-5*h(x-1))}
+}
+plot(g,10,n)
+plot(h,10)
+h(2)
+
+cek <- function(x){
+  sapply(x, g)
+}
+
+cek2 <- function(x){
+  sapply(x, h)
+}
+
+plot(cek,10)
+plot(cek2,4)
