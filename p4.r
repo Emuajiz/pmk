420cc589f39742a2b0a93316f09a8431e8166c74
Emuajiz <emuajiz@gmail.com>
2019-09-06 10:58:41 +0700

    Update p4.r

--- p4.r
+++ p4.r
@@ -3,12 +3,15 @@ tm = ((-1)^(n-1))/n
 f <- function(x){
   ((-1)^(x-1))/x
 }
+for(i in 1:10){
+  print(f(i))
+}
 sum(tm)
 log(2)
 
 g <- function(x){
   if(x>=1.0){
-    2^((-1*x)+4)*3
+    2^(4-x)*3^x
   }
 }
 
@@ -16,7 +19,7 @@ h <- function(x){
   if(x==1.0) {return(0.6)}
   else if(x>1.0) {return(-5*h(x-1))}
 }
-plot(g,10,n)
+plot(g,10)
 plot(h,10)
 h(2)
 
